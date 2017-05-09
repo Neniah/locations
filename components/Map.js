24a373648d841d3c29afaaaa6738733bb8bb8ba0
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-05-09 18:49:55 +0200

    map

---
+++ components/Map.js
@@ -0,0 +1,39 @@
+var React = require('react');
+
+var Map = React.createClass({
+
+  componentDidMount(){
+
+    this.componentDidUpdate();
+  },
+  componentDidUpdate(){
+    if(this.lastLat == this.props.lat && this.lastLng == this.props.lng){
+
+      return;
+    }
+
+    this.lastLat = this.props.lat;
+    this.lastLng = this.props.lng;
+
+    var map = new GMaps({
+      el: '#map',
+      lat: this.props.lat,
+      lng: this.props.lng
+    });
+
+    map.addMarker({
+      lat: this.props.lat,
+      lng: this.props.lng
+    });
+  },
+  render(){
+    return (
+      <div className="map-holde">
+        <p>Loading...</p>
+        <div id="map"></div>
+      </div>
+    );
+  }
+});
+
+module.exports = Map;
